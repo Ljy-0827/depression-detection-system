3e528d09837bbd1e46fd3ad99a9e36eb07a7a03f
Junyao Liu <liujunyao0827@163.com>
2024-01-22 02:10:16 +0800

    reading page: countdown open mic & camera, action visible

--- system-front-end/src/components/readingPage.vue
+++ system-front-end/src/components/readingPage.vue
@@ -33,7 +33,11 @@
     <div class="tool-text-box">
       <div class="tool-text">请您在确认音视频权限均已打开后，大声朗读屏幕上的两段短文。</div>
     </div>
-    <div class="media-box"></div>
+    <div class="media-box">
+      <div class="media-instruction" v-if="!isCameraOpen" style="margin-top: 2.3vh">音视频将在{{this.countdown}}秒后</div>
+      <div class="media-instruction" v-if="!isCameraOpen">自动尝试开启</div>
+      <video ref="videoElement" autoplay class="camera" v-if="isCameraOpen || isTryOpenCamera" muted></video>
+    </div>
   </div>
   <div class="read-text-container">
     <div class="read-text-instruction" v-if="currentPage === 0">
@@ -53,24 +57,76 @@
 
 <script>
 import { text1, text2 } from "@/utils.js";
+import { ElMessage } from "element-plus";
 
 export default {
   name: "readingPage",
   data(){
     return{
+      countdown: 3,
+      timer: null,
+
       readText1: text1,
       readText2: text2,
 
       isAllowNextPage: false,
       isAllowStop: false,
       isAllowStart: true,
-
-      isCameraOpen: false,
-      isMicOpen: true,
       currentPage: 0,
+
+      isTryOpenCamera: false,
+      isCameraOpen: false,
+      isMicOpen: false,
+      isRecording: false,
+
+      mediaStream: null,
+      mediaRecorder: null,
+      chunks: [],
     }
   },
+
+  mounted() {
+    this.startCountdown();
+  },
+
   methods:{
+    startCountdown() {
+      this.countdown = 3;
+      this.timer = setInterval(() => {
+        if (this.countdown > 0) {
+          this.countdown--;
+        } else {
+          clearInterval(this.timer);  // 清除计时器
+          this.openCamera();
+        }
+      }, 1000);
+    },
+
+    async openCamera() {
+      try {
+        this.isTryOpenCamera = true;
+        this.mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        this.$refs.videoElement.srcObject = this.mediaStream;
+        this.isTryOpenCamera = false;
+
+        const videoTrack = this.mediaStream.getVideoTracks()[0];
+        if (videoTrack.enabled) {
+          this.isCameraOpen = true;
+        }
+
+        const audioTrack = this.mediaStream.getAudioTracks()[0];
+        if (audioTrack.enabled) {
+          this.isMicOpen = true;
+        }
+
+      } catch (error) {
+        this.isCameraOpen = false;
+        ElMessage.error('无法访问音视频');
+        console.error('无法访问音视频', error);
+      }
+    },
+
+
     startReading(){
       this.isAllowStart = false;
       this.isAllowNextPage = true;
---
+++ system-front-end/src/main.js
@@ -0,0 +1,46 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import landingPage from '@/components/landingPage.vue';
+import readingPage from "@/components/readingPage.vue";
+import interviewPage from "@/components/interviewPage.vue";
+
+const myRouter = createRouter({
+    mode: 'history',
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [{
+            path: '/',
+            name: 'landing-page',
+            component: landingPage,
+        },
+        {
+            path: '/read',
+            name: 'reading-page',
+            component: readingPage,
+        },
+        {
+            path: '/interview',
+            name: 'interview-page',
+            component: interviewPage,
+        }]
+})
+
+const myApp = createApp(App)
+myApp.use(myRouter)
+
+myApp.mount('#app')
+
+myApp.use(ElementPlus, {
+    locale: zhCn,
+})
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    myApp.component(key, component)
+}
